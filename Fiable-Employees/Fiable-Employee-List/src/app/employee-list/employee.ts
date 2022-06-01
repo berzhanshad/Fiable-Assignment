25a21d29b3c04051e3864b8c38dc941e117e464c
berzhan.shad <90727526+berzhanshad@users.noreply.github.com>
2022-05-31 23:51:54 -0400

    Add files via upload

---
+++ Fiable-Employees/Fiable-Employee-List/src/app/employee-list/employee.ts
@@ -0,0 +1,9 @@
+export class Employee {
+    constructor( public EmployeeID:string,
+        public Name:string,
+        public Salary: number,
+        public Designation: string,
+        public Credit: number,
+        public EligibleforPromotion: string){
+    }
+}
